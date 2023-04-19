1ed1ce0f6805bf81491a69c585a76d509fd3c730
marcellinrabe <marcellinp20.aps1a@gmail.com>
2023-04-19 07:03:11 +0300

    DEV: add check database connection log

--- api/database/connection.py
+++ api/database/connection.py
@@ -8,6 +8,14 @@ class DBConnection:
     def __init__(self, uri, db_name):
         self.client = MongoClient(uri)
         self.db = self.client[db_name]
+        self.check_connection()
+
+    def check_connection(self):
+        db_names = self.client.list_database_names()
+        if db_names:
+            print("database connection successfully")
+        else:
+            print("database connection failed")
 
     def find_one(self, collection_name, filters):
         collection = self.db[collection_name]
